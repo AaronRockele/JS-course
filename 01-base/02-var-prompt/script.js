a981bf69e03cf6f209bc5a9e94adea6525bf6bd6
Aaron Rockele <aaron_rockele@MacBook-Pro-van-Aaron.local>
2019-09-03 10:24:18 +0200

    stopped calling the function

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -1,8 +1,16 @@
+/* becode/javascript
+ *
+ * /01-base/02-var-prompt/script.js - 1.2: var & prompt
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
 
-function alertMessage() {
-    var person = prompt("Please enter your name");
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+var person = prompt("Please enter your name");
     if (person != null) {
     alert('Hello' +', '+ person);
-}}
-
-alertMessage();
+}})();
