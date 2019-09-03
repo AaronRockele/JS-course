3c86024e14da65ead9fb841e8e9ffb29f9cbd433
Aaron Rockele <aaron_rockele@MacBook-Pro-van-Aaron.local>
2019-09-03 11:27:43 +0200

    1.3 complete

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -1,16 +1,9 @@
-/* becode/javascript
- *
- * /01-base/03-prompt-if/script.js - 1.3: promp & if
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
-
-  // your code here
-
+  var cake = prompt("do you like cake?");
+  if (cake === 'yes') {
+  alert('congratz!');
+} else {
+  alert('yay, more cake for me!');
+}
 })();
